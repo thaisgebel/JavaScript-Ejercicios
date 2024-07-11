2000a0079f34c0d52984fd6e281c885ce0af8d61
Thais <thais.gebel@gm2dev.com>
2024-07-11 09:52:12 -0300

    feat(excercises): add primitive types + comments + reserved words - excercises

---
+++ Primitive Types + Comments + Reserved words - Excercises/Primitive Types + Comments + Reserved words - Excercises.js
@@ -0,0 +1,46 @@
+/*Ejercicio 1: Palabras Reservadas
+
+Escribe un comentario que liste al menos 8 palabras reservadas en JavaScript.*/
+
+/*breack-byte-case-catch-eval-final-do-float*/
+
+/*Ejercicio 2: Tipos de Datos Primitivos
+
+Escribe un comentario que liste todos los tipos de datos primitivos en JavaScript.
+Usa console.log para mostrar un ejemplo de cada tipo de dato primitivo.
+https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_datatypes1 (no hacer el de objetos)*/
+
+/*Symbol-Function-String-Any other object-Number-BigInt-Boolean-Undefined-Null que es un caso especial para cada OBJECT*/
+
+console.log(typeof 37)
+console.log(typeof "Beach")
+console.log(typeof false)
+console.log(typeof undefined)
+console.log(typeof [2,1,4])
+console.log(typeof 42n)
+console.log(typeof function(){})
+console.log(typeof null)
+
+/*Ejercicio 3: Práctica Combinada
+
+Escribe un bloque de código que haga lo siguiente:
+
+Incluya un comentario explicativo al inicio.
+Liste varias palabras reservadas en un comentario.
+Use console.log para mostrar ejemplos de diferentes tipos de datos primitivos.*/
+
+/* En el siguiente console.log voy a mostrar ejemplos de diferentes tipos de datos primitivos, declarando previamente
+ las variables.*/
+
+/*abstract arguments await boolean break byte case	catch char class const continue debugger default delete	do
+double	else enum eval export extends false	final finally float	for	function goto if implements	import in instanceof
+int	interface let long native new null package	private	protected public return	short static super* switch synchronized this
+throw throws transient true try	typeof var void volatile while	with yield*/
+
+let numero=37
+let Bigint=37n
+let String="Tomorrow"
+let Boolean= true
+let Symbol= ("fifi")
+let Undefined = "Soy algo no definido"
+let object = ["soy un objeto"]
