d11274b2105d60d9d0bb089777b9ab7fcefd2f8d
Thais <thais.gebel@gm2dev.com>
2024-07-11 09:36:13 -0300

    feat(excercise):add console-excercise

---
+++ Console - Excercises/ConsoleExcercises.js
@@ -0,0 +1,49 @@
+/*Ejercicio 1: Mensajes Básicos
+
+Muestra un mensaje en la consola que diga "Hola, mundo!".
+Muestra en la consola tu nombre.
+Muestra en la consola tu ciudad y país */
+
+console.log("Hola Mundo!");
+console.log("Maria Thais Gebel Lucas")
+console.log("Buenos Aires, Argentina")
+
+/*Ejercicio 2: Diferentes Niveles de Mensajes
+
+Muestra un mensaje normal en la consola diciendo "Bienvenido al sistema".
+Muestra un mensaje de información en la consola diciendo "Cargando aplicación...".
+Muestra una advertencia en la consola diciendo "Advertencia: Espacio en disco bajo".
+Muestra un error en la consola diciendo "Error: No se pudo conectar al servidor".*/
+
+console.log("Bienvenido al sistema")
+console.info("Cargando aplicación...")
+console.warn("Advertencia: Espacio en disco bajo")
+console.error("Error: No se pudo conectar al servidor")
+
+/*Ejercicio 3: Pirámide.
+
+Crea un script que imprima tres líneas de estrellas * para que parezca una pirámide en la pantalla.
+Ejecute y realice cambios hasta que vea que funciona correctamente.*/
+
+console.log("   *")
+console.log("  * *")
+console.log(" * * *")
+console.log("* * * *")
+
+/*Ejercicio 4: Debugger
+
+Imprimir en la consola los números del 1 al 10, haciendo que el codigo se pause en luego de mostrar el número 5.
+Ejecutar paso a paso el resto de los numeros.
+*/
+
+console.log("1")
+console.log("2")
+console.log("3")
+console.log("4")
+console.log("5")
+debugger
+console.log("6")
+console.log("7")
+console.log("8")
+console.log("9")
+console.log("10")
