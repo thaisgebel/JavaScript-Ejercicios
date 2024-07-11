c2302fd862992c9ce73e9a727a6f8c1f6c1f49e4
Thais <thais.gebel@gm2dev.com>
2024-07-11 10:07:27 -0300

    feat(excercises) add operators - excercises

---
+++ Operators - Excercises/Operators - Excercises.js
@@ -0,0 +1,72 @@
+/*Ejercicio 1: Operadores Aritméticos
+
+https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_operators1 (menos 4 y 5)
+https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_comparisons1 (menos el 4)                     
+Usa console.log para realizar y mostrar las siguientes operaciones:
+
+La suma de 15 y 30.
+La resta de 100 menos 45.
+El producto de 8 y 7.
+La división de 56 entre 8.
+El módulo de 27 entre 4*/
+
+console.log (15+30)
+console.log (100-45)
+console.log (8*7)
+console.log (56/8)
+console.log (27%4)
+
+/*Ejercicio 2: Valores y Expresiones
+
+Usa console.log para mostrar el resultado de las siguientes expresiones:
+La suma entre 20 y 22.
+La concatenación de las cadenas "Hola" y "Mundo".
+El resultado de multiplicar 5 por la suma de 3 y 2.*/
+
+console.log (20+22)
+console.log ("Hola"+ " Mundo")
+console.log ((3+2)*5)
+
+/*Ejercicio 3: Operadores de Cadena y Número
+
+Usa console.log para concatenar una cadena y un número.
+Usa console.log para concatenar una cadena, un número y volver a concatenar una cadena.
+Usa console.log para mostrar una cadena que incluya el resultado de una operación aritmética.*/
+
+console.log ("Hola "+123)
+console.log ("holi "+2+" chau")
+console.log ("holi "+(2+7))
+
+/*Ejercicio 4: Operadores de Comparación
+
+Escribir código que muestre en consola:
+‘True’ usando == y ===.
+‘False’ usando == y ===.
+‘True’ usando != y !==.
+‘False’ usando != y !==.
+‘True’ usando >, <, >=, y <=.
+‘False’ usando >, <, >=, y <=.
+‘True’ usando == y ===.
+‘False’ usando == y ===.*/
+
+console.log (5==5)
+console.log (5===5)
+
+console.log (5=="cinco")
+console.log (5=="cinco")
+
+console.log (5!=10)
+console.log (5!==10)
+
+console.log (5!=5)
+console.log (5!==5)
+
+console.log (20>10)
+console.log (5<10)
+console.log (5<=10)
+console.log (20>=10)
+
+console.log (20<10)
+console.log (5>10)
+console.log (5>=10)
+console.log (20<=10)
